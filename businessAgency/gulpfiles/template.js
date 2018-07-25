8932af24e4caf6e4162bee1a425de1c7215dd297
zpawn <ilia.makarov@me.com>
2018-07-25 23:58:09 +0300

    Update gulp tasks

--- businessAgency/gulpfiles/template.js
+++ businessAgency/gulpfiles/template.js
@@ -7,7 +7,7 @@ const gulp = require('gulp'),
         .argv,
 
     config = {
-        src: './src/tpl/index.pug',
+        src: './src/tpl/*.pug',
         dest: './build/.',
         pug: {},
         plumber: {
