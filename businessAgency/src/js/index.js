f1e53c00304ea575580d6de52d10a348f8adee87
zpawn <ilia.makarov@me.com>
2018-07-26 00:01:07 +0300

    Create base layout template

--- businessAgency/src/js/index.js
+++ businessAgency/src/js/index.js
@@ -1,1 +1,22 @@
-((...arg)=> setTimeout(() => (console.log('>>>', ...arg)), 1500))(1, 2, 3, 4, 5);
+const loginForm = document.forms.loginForm;
+
+////
+
+document.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('[click]');
+
+    switch (e.target.id) {
+        case 'loginFormReset':
+            loginForm.reset();
+            break;
+        case 'loginFormSubmit':
+            loginForm.submit();
+            break;
+    }
+});
+
+document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('[submit]');
+});
